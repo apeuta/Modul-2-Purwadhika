c628f9d43fdde6d5af335f98f7fe7ef86719d66b
apeuta <andra.p.utama@gmail.com>
2019-11-18 14:05:28 +0700

    Modul 2.0 v2.1

--- Day 3/nov18a.py
+++ Day 3/nov18a.py
@@ -1,13 +1,18 @@
+'''
+Money Value Converter
+'''
 import requests
 import json
 bank = input("Masukkan nama bank : ")
 url = f"https://kurs.web.id/api/v1/{bank.lower()}"
 data = requests.get(url).json()
+print (data["beli"])
+print (data ["jual"])
 X = int(input("Mau tukar ke rupiah (1) atau ke dolar (2) : "))
 Y = int(input("Masukkan nominal yang akan ditukar : "))
 if X == 2 :
-    print (f"Harga beli dari bank {bank.upper()} saat ini adalah {data['beli']}")
-    print (f"IDR {Y} = USD {Y/(int(data['beli']))}")
+    print (f"Harga jual dari bank {bank.upper()} saat ini adalah {data['jual']}")
+    print (f"IDR {Y} = USD {round((Y/int(data['jual'])),2)}")
 else:
-    print (f"Harga beli dari bank {bank.upper()} saat ini adalah {data['jual']}")
-    print (f"USD {Y} = IDR {Y*(int(data['jual']))}")
+    print (f"Harga beli dari bank {bank.upper()} saat ini adalah {data['beli']}")
+    print (f"USD {Y} = IDR {Y*(int(data['beli']))}")
